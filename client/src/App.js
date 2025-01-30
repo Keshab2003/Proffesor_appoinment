710e443d9e289a504e7596d4643c8482fb24f7f0
Keshab Chaudhary <keshabchaudhary23132@gmail.com>
2025-01-30 17:00:27 +0530

    create reactapp for client side and included CDN for bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "bg-dark">
+      <h2 className = "text-danger">hello Keshab This side</h2> 
+      </div>  
+    </div>
+  );
+}
+
+export default App;
