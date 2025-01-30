c1fc853df09279ac5b06f06b174767d9081381ac
Keshab Chaudhary <keshabchaudhary23132@gmail.com>
2025-01-30 17:09:55 +0530

    all the required paths are added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import Homepage from "./pages/Homepage";
+import Registerpage from "./pages/Registerpage";
+import Loginpage from "./pages/Loginpage";
 
 function App() {
   return (
-    <div className="App">
-      <div className = "bg-dark">
-      <h2 className = "text-danger">hello Keshab This side</h2> 
-      </div>  
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/register" element={<Registerpage/>}/>
+          <Route path="/login" element={<Loginpage/>}/>
+        </Routes>
+      </BrowserRouter>
+        
+    </>
   );
 }
 
