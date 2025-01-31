bc9cb4da994cd0da6e4efddd285077b39dc0755d
Keshab Chaudhary <keshabchaudhary23132@gmail.com>
2025-01-31 15:59:40 +0530

    code for getting user data at home page are added!

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -56,4 +56,30 @@ const loginController = async (req , res) => {
         res.status(500).send({message : `Error in login controller ${error.message}`});
     }
 }
-module.exports = {loginController,registerController};
+
+
+const getUserDataController =async (req , res) => {
+    try{
+        const user = await userModel.findOne({_id : req.body.userId})
+        if(!user){
+            return res.status(200).send({
+                message:"User not found",
+                success:false
+            })
+        }
+        else{
+            res.status(200).send({
+                message:"User data",
+                success:true,
+                data:{
+                    name:user.name,
+                    email:user.email
+                }
+            })
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).send({message : `Error in getUserDataController ${error.message}` , success:false , error});
+    }
+};
+module.exports = {loginController,registerController , getUserDataController};
