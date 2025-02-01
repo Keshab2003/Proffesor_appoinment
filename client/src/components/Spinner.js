60f258fbbcbd88e6b7b0ecaa0581666387d7d6ab
Keshab Chaudhary <keshabchaudhary23132@gmail.com>
2025-02-01 19:17:40 +0530

    all the functionalities realted to spinner and loading functions has been added!

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+//     <div className="d-flex justify-content-center">
+//   <div className="spinner-border" role="status">
+//     <span className="sr-only">Loading...</span>
+//   </div>
+// </div>
+
+
+
+<div className="d-flex justify-content-center spinner">
+<div className="spinner-border text-secondary" role="status">
+  <span className="sr-only">Loading...</span>
+</div>
+</div>
+
+
+
+  )
+}
+
+export default Spinner;
